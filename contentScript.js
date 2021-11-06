fc4596304c7360b5957385be1b30305058632bd9
Sébastien Vallet <sebastien.vallet89@gmail.com>
2021-11-06 23:30:24 +0100

    fix slugs

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,23 @@
 if (typeof init === 'undefined') {
   const init = function () {
-    const toWatchElementList = document.querySelectorAll(".cd-timeline-block");
-    for (const toWatchElement of toWatchElementList) {
-      const serie = toWatchElement.querySelector(".cd-timeline-content-title-small").parentElement.getAttribute("href").split("/").pop();
-      const metadata = toWatchElement.querySelector("span.title b").textContent;
-      const season = metadata.split("E")[0].replace('S', '').replace(/^0+/, '');
-      const episode = metadata.split("E")[1].replace(/^0+/, '');
-
-      const buttonsContainer = toWatchElement.querySelector(".cd-timeline-content-buttons");
-      const button = buildButton({ serie, season, episode });
-      buttonsContainer.appendChild(button);
-    }
+    executeInternal();
   }
   init();
+}
+
+function executeInternal() {
+  const toWatchElementList = document.querySelectorAll(".cd-timeline-block");
+  for (const toWatchElement of toWatchElementList) {
+    const serieRaw = toWatchElement.querySelector(".cd-timeline-content-title-small").innerHTML.split("<small>")[0];
+    const serie = slugify(serieRaw, { remove: /[':]/g, lower: true });
+    const metadata = toWatchElement.querySelector("span.title b").textContent;
+    const season = metadata.split("E")[0].replace('S', '').replace(/^0+/, '');
+    const episode = metadata.split("E")[1].replace(/^0+/, '');
+
+    const buttonsContainer = toWatchElement.querySelector(".cd-timeline-content-buttons");
+    const button = buildButton({ serie, season, episode });
+    buttonsContainer.appendChild(button);
+  }
 }
 
 function buildButton({ serie, season, episode }) {
